0e933c350562a1498532e4ab02a818bc7a1b43c3
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-22 17:54:54 +0800

    Finished

--- first_and_last_number.py
+++ first_and_last_number.py
@@ -1,16 +1,14 @@
-def first_last_same(numberList):
-    print("Given list:", numberList)
-    
-    first_num = numberList[0]
-    last_num = numberList[-1]
-    
-    if first_num == last_num:
-        return True
-    else:
-        return False
+# Check if the first and last number of a list is the same
 
-numbers_x = [10, 20, 30, 40, 10]
-print("result is", first_last_same(numbers_x))
 
-numbers_y = [75, 65, 35, 75, 30]
-print("result is", first_last_same(numbers_y))
+# Function
+def same_first_and_last_number(input_list):
+    print("Given list:", input_list)
+    first_num, last_num = input_list[0], input_list[-1]
+    return first_num == last_num
+
+numbers_one = [1, 2, 3, 4, 5]
+print("Result is", same_first_and_last_number(numbers_one))
+
+numbers_two = [6, 7, 8, 9, 6]
+print("Result is", same_first_and_last_number(numbers_two))
